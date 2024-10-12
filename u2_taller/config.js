f194ee0e1eb99abf27c2e5d5e34cfd87b8f6e1bb
FPabl0 <f.pablo1@hotmail.com>
2024-10-11 21:42:27 -0500

    fix container deployment

--- u2_taller/config.js
+++ u2_taller/config.js
@@ -1,7 +1,6 @@
 const config = {
     PORT: 3000,
-    // DB_URL: 'mongodb://dba:12345@mongodb:27017/miapp?authSource=admin'
-    DB_URL: 'mongodb://dba:12345@localhost:27017/miapp?authSource=admin'
+    DB_URL: 'mongodb://dba:12345@mongodb:27017/miapp?authSource=admin'
 }
 
 module.exports = config
